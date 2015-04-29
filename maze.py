59fcfd65220eec35c522473c11f5c238d948be51
wlong0827 <wlong0827@gmail.com>
2015-04-28 22:57:07 -0400

    Started maze algorithm

--- maze.py
+++ maze.py
@@ -1,3 +1,6 @@
+from mazegraphics import *
+from module import *
+"""
 from random import shuffle, randrange
  
 def make_maze(w = 16, h = 8):
@@ -21,3 +24,38 @@ def make_maze(w = 16, h = 8):
 		print(''.join(a + ['\n'] + b))
  
 make_maze()
+"""
+
+import random
+
+def make_maze(w, h):
+    edges = [[0 for x in range(w)] for x in range(h)]
+    max = 100
+    for col in range(w):
+        for row in range(h):
+            edges[col][row] = (random.randint(1,max), True)
+    print edges
+    
+    def in_grid(x, y):
+        return x >= 0 and y >= 0 and x < w and y < h
+    
+    def adjacent(x, y):
+        results = []
+        if in_grid(x+1,y):
+            results.append(edges[x+1][y])
+        if in_grid(x-1,y):
+            results.append(edges[x-1][y])
+        if in_grid(x,y+1):
+            results.append(edges[x][y+1])
+        if in_grid(x,y-1):
+            results.append(edges[x][y-1])
+        return results
+
+    for cur_col in range(w):
+        for cur_row in range(h):
+            adjacents = adjacent(cur_col, cur_row)
+            minimum = min(adjacents)
+            minimum[1] = False
+            print adjacents
+    
+make_maze(5,5)
